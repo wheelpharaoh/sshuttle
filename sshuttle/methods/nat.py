022605f8a8dd5c224bd10debe0d3e978c5291b57
Itamar Turner-Trauring <itamar@datawire.io>
2017-07-26 11:18:02 -0400

    Encoding.

--- sshuttle/methods/nat.py
+++ sshuttle/methods/nat.py
@@ -78,7 +78,7 @@ class Method(BaseMethod):
             msg = e.output
             # If there are no entries this gives exit code of 1.
             pass
-        debug1(msg)
+        debug1(str(msg, "utf-8"))
 
     def restore_firewall(self, port, family, udp):
         # only ipv4 supported with NAT
